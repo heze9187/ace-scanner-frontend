a1002cd2a3dc4f5cf9dd68b8c47dc65f13b0a70e
heze9187 <eriche9187@gmail.com>
2025-04-26 20:31:53 -0400

    update

--- src/api/api.js
+++ src/api/api.js
@@ -72,7 +72,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const api = axios.create({
   baseURL: isProduction
-    ? 'https://ace-scanner-backend.onrender.com/api/'
+    ? 'https://api.ace-scanner.com/api/'
     : 'http://localhost:8000/api/',
   withCredentials: true,
   xsrfCookieName: 'csrftoken',
